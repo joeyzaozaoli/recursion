5d194e8aae66d06e80ef16e81e0861491448361c
Joey Li <joeyzaozaoli@gmail.com>
2017-05-07 21:01:06 -0700

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (obj === null || typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return obj + '';
+  } else if (typeof(obj) === 'string') {
+  	return '"' + obj + '"'
+  }
+
+  if (Array.isArray(obj)) {
+  	
+    for (var i = 0, str = '['; i < obj.length; i++) {
+  	  if (obj[i] === undefined || typeof(obj[i]) === 'function') {
+  	  	return '[]';
+  	  }
+      str += stringifyJSON(obj[i]) + ','
+  	}
+  	return str.replace(/,$/, '') + ']'; 
+
+  } else if (typeof(obj) === 'object') {
+
+    var arr = Object.keys(obj);
+
+    for (var i = 0, str = '{'; i < arr.length; i++) {
+      if (obj[arr[i]] === undefined || typeof(obj[arr[i]]) === 'function') {
+        return '{}';
+      }
+      str += stringifyJSON(arr[i]) + ':' + stringifyJSON(obj[arr[i]]) + ','
+    }
+    return str.replace(/,$/, '') + '}';
+    
+  }
 };
