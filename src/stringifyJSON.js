45d4136a885f3eb2e6cf368965cb2333570e0c41
Joey Li <joeyzaozaoli@gmail.com>
2017-12-17 10:56:32 -0800

    Re-do stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,34 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  var type = typeof(obj);
 
-  if (obj === null || typeof(obj) === 'boolean' || typeof(obj) === 'number') {
-    return obj + '';
-  } else if (typeof(obj) === 'string') {
-  	return '"' + obj + '"';
+  if (type === 'number' || type === 'boolean' || obj === null) {
+    return `${obj}`;
+  } else if (type === 'string') {
+    return `"${obj}"`;
   }
 
-  if (Array.isArray(obj)) {
+  if (obj.constructor === Array) {
+    // create a string to represent result
+    var result = '[';
 
-    for (var i = 0, str = '['; i < obj.length; i++) {
-  	  if (obj[i] === undefined || typeof(obj[i]) === 'function') {
-  	  	return '[]';
-  	  }
-      str += stringifyJSON(obj[i]) + ',';
-  	}
-  	return str.replace(/,$/, '') + ']';
-
-  } else if (typeof(obj) === 'object') {
-
-    var arr = Object.keys(obj);
-
-    for (var i = 0, str = '{'; i < arr.length; i++) {
-      if (obj[arr[i]] === undefined || typeof(obj[arr[i]]) === 'function') {
-        return '{}';
+    // for each primitive value in array
+    var length = obj.length;
+    for (var i = 0; i < length; i++) {
+      // call above function and concat what is returned from function to result string
+      if (i !== length-1) {
+        result += `${stringifyJSON(obj[i])},`;
+      } else {
+        result += stringifyJSON(obj[i]);
       }
-      str += stringifyJSON(arr[i]) + ':' + stringifyJSON(obj[arr[i]]) + ',';
     }
-    return str.replace(/,$/, '') + '}';
 
+    // concat result string with ']'
+    result += ']';
+    // return result string
+    return result;
+  }
+
+  if (obj.constructor === Object) {
+    // create a string to represent result
+    var result = '{';
+
+    // for each key-value pair in object
+    for (var key in obj) {
+      var value = obj[key];
+      if (value === undefined) {
+        return '{}';
+      } else {
+        // call above function and concat what is returned from function to result string
+        result += `${stringifyJSON(key)}:${stringifyJSON(value)},`;
+      }
+    }
+
+    // remove last comma
+    result = result.replace(/,$/, '');
+    // concat result string with '}'
+    result += '}';
+    // return result string
+    return result;
   }
 };
